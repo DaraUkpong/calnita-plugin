d5f3dea269e9390b7c4c53e3651fb40aa54a0e6e
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-10-11 12:02:08 +0100

    moved the update logic from the page.tsx to the QuestionnaireFlow component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,38 +1,14 @@
 "use client";
 
-import Pill from "@/components/Pill";
-import { useSession } from "next-auth/react";
-import React, { useState } from "react";
+import React from "react";
 import { QuestionnaireProvider } from "./questions/context";
 import { QuestionnaireFlow } from "./questions/QuestionnaireFlow";
 
 const ProfilePage: React.FC = () => {
-  const { data: session } = useSession();
-  const [profileData, setProfileData] = useState(session?.user);
-
-  const handleUpdate = async () => {
-    const response = await fetch("/api/update-user", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(profileData),
-    });
-
-    if (response.ok) alert("Profile updated successfully");
-  };
-
   return (
-    <div className="w-full h-full flex flex-col text-black px-[40px] ">
-      <QuestionnaireProvider>
-        <QuestionnaireFlow />
-      </QuestionnaireProvider>
-
-      <button
-        onClick={handleUpdate}
-        className="w-[204px] h-[69px] rounded-[20px] bg-black text-white text-[16px] font-semibold "
-      >
-        Next
-      </button>
-    </div>
+    <QuestionnaireProvider>
+      <QuestionnaireFlow />
+    </QuestionnaireProvider>
   );
 };
 
--- src/app/profile/questions/QuestionnaireFlow.tsx
+++ src/app/profile/questions/QuestionnaireFlow.tsx
@@ -1,6 +1,8 @@
 import ProgressBar from "@/components/ProgressBar";
 import { useQuestionnaire } from "./context";
 import React from "react";
+import { updateUser } from "@/services/user";
+import { useSession } from "next-auth/react";
 
 export function QuestionnaireFlow() {
   const {
@@ -21,8 +23,22 @@ export function QuestionnaireFlow() {
       ? ((currentQuestionIndex + 1) / totalQuestions) * 100
       : 0; // Avoid division by zero
 
+  const { data: session } = useSession();
+
+  const handleFinish = async (formData: any) => {
+    alert("Questionnaire Completed!");
+    return;
+    if (!session) return;
+    const response = await updateUser({
+      formData,
+      userId: session?.user?.id,
+    });
+
+    if (response?.ok) alert("Profile updated successfully");
+  };
+
   return (
-    <div>
+    <div className="w-full h-full flex flex-col text-black px-[40px]">
       {currentQuestionIndex > 0 && (
         <>
           <div className="text-[12px] text-[#222222] flex justify-between mt-[50px]">
@@ -39,32 +55,34 @@ export function QuestionnaireFlow() {
         </>
       )}
 
-      {/* Dynamically render the current question */}
       <CurrentQuestionComponent />
 
-      {/* Navigation buttons */}
-      <button
-        onClick={goToPreviousQuestion}
-        disabled={currentQuestionIndex === 0}
-      >
-        Back
-      </button>
-      <button
-        onClick={goToNextQuestion}
-        disabled={currentQuestionIndex === mergedQuestions.length - 1}
-      >
-        Next
-      </button>
-
-      {/* Display Finish button when on the last question */}
-      {currentQuestionIndex === mergedQuestions.length - 1 && (
+      {/* Navigation Buttons (Fixed at 46px from the bottom) */}
+      <div className="p-4 flex justify-between fixed bottom-[46px] left-0 right-0 bg-white">
         <button
-          onClick={() => alert("Questionnaire Completed!")}
-          style={{ marginLeft: "10px" }}
+          onClick={goToPreviousQuestion}
+          disabled={currentQuestionIndex === 0}
+          className="bg-gray-200 px-4 py-2 rounded"
         >
-          Finish
+          Back
         </button>
-      )}
+        {currentQuestionIndex === mergedQuestions.length - 1 ? (
+          <button
+            onClick={handleFinish}
+            className="w-[204px] h-[69px] rounded-[20px] bg-black text-white text-[16px] font-semibold "
+          >
+            Finish
+          </button>
+        ) : (
+          <button
+            onClick={goToNextQuestion}
+            disabled={currentQuestionIndex === mergedQuestions.length - 1}
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Next
+          </button>
+        )}
+      </div>
     </div>
   );
 }
