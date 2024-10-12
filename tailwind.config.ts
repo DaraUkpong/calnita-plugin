e38c64712eab48654df598ab0cdababc5fdb0d5d
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-10-12 09:13:26 +0100

    added custom breakpoints in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,16 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        //"iframe-sm": "375px", // Small mobile (iPhone 12, 13, etc.)
+        //"iframe-md": "430px", // Larger mobiles (iPhone 14 Pro Max, etc.)
+        // "iframe-lg": "768px", // Small desktop or tablet
+        // "iframe-sm": { max: "430px" }, // Small mobile devices, up to 430px
+        //"iframe-md": { max: "452px" }, // Anything smaller than the full iframe size
+        //"iframe-lg": "453px", // Large screens from 453px upwards (desktop)
+        "iframe-sm": { max: "452px" }, // Mobile devices below 453px
+        "iframe-lg": "453px",
+      },
     },
   },
   plugins: [],
