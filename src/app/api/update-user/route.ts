3b028c02f6a3939dca625fd11b59ea5223550cf6
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-10-25 17:24:51 +0100

    refactored the update-user api  route

--- src/app/api/update-user/route.ts
+++ src/app/api/update-user/route.ts
@@ -1,7 +1,9 @@
-import { graphqlClient, UPDATE_ME_MUTATION } from "@/services/graphql";
+import { graphqlClient } from "@/services/graphql";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/authOption";
+import { updateMe } from "@/services/user/user";
+import { mapResponsesToUserInput } from "@/services/graphql/mappings";
 
 export async function POST(req: NextRequest) {
   try {
@@ -12,22 +14,25 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
-    // Get the request body
-    const { input } = await req.json();
+    // Get the request body and destructure the fields
+    const reqBody = await req.json();
 
-    // Optionally, use the session data to authenticate the request to your GraphQL server
-    // For example, set it in headers or some other authentication mechanism
-    // Assuming you have an access token in the session, use it instead of refresh token
-    if (session.accessToken) {
-      graphqlClient.setHeader("Authorization", `Bearer ${session.accessToken}`);
+    // Log for debugging
+    //console.log("Received body:", reqBody);
+
+    //return NextResponse.json({ status: 200 });
+
+    const accessToken = session.user.accessToken;
+    //console.log(accessToken);
+
+    if (accessToken) {
+      graphqlClient.setHeader("Authorization", `Bearer ${accessToken}`);
     }
 
-    // Perform the GraphQL request
-    const data: any = await graphqlClient.request(UPDATE_ME_MUTATION, {
-      input,
-    });
+    const input = mapResponsesToUserInput(reqBody);
+    const data = await updateMe(input);
 
-    return NextResponse.json(data.updateMe, { status: 200 });
+    return NextResponse.json(data, { status: 200 });
   } catch (error) {
     console.error("Error updating user:", error);
     return NextResponse.json(
