e9e78e5ca87d861e2a6bc9eda2f21fddfdcc931e
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-10-25 16:59:03 +0100

    removed unnecessary  try catch around updateUser function

--- src/services/user/updateUser.ts
+++ src/services/user/updateUser.ts
@@ -4,15 +4,12 @@ interface UpdateUserParams {
 }
 
 export const updateUser = async ({ formData, userId }: UpdateUserParams) => {
-  try {
-    const response = await fetch("/api/update-user", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ ...formData, userId }),
-    });
-    return response;
-  } catch (error) {
-    console.error("Failed to update user data:", error);
-    return null;
-  }
+  //console.log("Form data being sent to API:", formData);
+  //return;
+  const response = await fetch("/api/update-user", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ...formData, userId }),
+  });
+  return response;
 };
