604e36eaf235f808f89bbce3554f408e0d85ab7c
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-10-26 01:19:34 +0100

    commented out unused Image import

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,6 @@
 import { Product } from "@/types";
 import { motion } from "framer-motion";
-import Image from "next/image";
+//import Image from "next/image";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
