c3ec884b5fe9cff3ae143011895d639a47e6576f
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-10-26 19:07:44 +0100

    removed the manual typing of the return type in graphql-request's client usage as the type is inferred from the TypedDocumentNode

--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -5,19 +5,17 @@ import {
   REFRESH_TOKEN_MUTATION,
   REQUEST_OTP_MUTATION,
 } from "../graphql/mutations";
-import { UserMutationResponse } from "../graphql/types";
 
 export const authenticateWithEmail = async (email: string, otp: string) => {
-  const response = await graphqlClient.request<{
-    userEmailAuth: UserMutationResponse;
-  }>(EMAIL_AUTH_MUTATION, { email, otp });
+  const response = await graphqlClient.request(EMAIL_AUTH_MUTATION, {
+    email,
+    otp,
+  });
   return response.userEmailAuth;
 };
 
 export const requestOTP = async (email: string) => {
-  const response = await graphqlClient.request<{
-    requestOTP: UserMutationResponse;
-  }>(REQUEST_OTP_MUTATION, { email });
+  const response = await graphqlClient.request(REQUEST_OTP_MUTATION, { email });
   return response.requestOTP;
 };
 
@@ -25,15 +23,16 @@ export const authenticateWithGoogle = async (
   googleIdToken?: string,
   googleAuthCode?: string
 ) => {
-  const response = await graphqlClient.request<{
-    userGoogleAuth: UserMutationResponse;
-  }>(GOOGLE_AUTH_MUTATION, { googleIdToken, googleAuthCode });
+  const response = await graphqlClient.request(GOOGLE_AUTH_MUTATION, {
+    googleIdToken,
+    googleAuthCode,
+  });
   return response.userGoogleAuth;
 };
 
 export const refreshToken = async (refreshToken: string) => {
-  const response = await graphqlClient.request<{
-    userRefreshToken: UserMutationResponse;
-  }>(REFRESH_TOKEN_MUTATION, { refreshToken });
+  const response = await graphqlClient.request(REFRESH_TOKEN_MUTATION, {
+    refreshToken,
+  });
   return response.userRefreshToken;
 };
--- src/services/user/user.ts
+++ src/services/user/user.ts
@@ -1,28 +1,23 @@
 import { graphqlClient } from "../graphql";
 import { UPDATE_ME_MUTATION } from "../graphql/mutations";
 import { ME_QUERY, USER_QUERY } from "../graphql/queries";
-import {
-  UpdateUserInput,
-  UserQueryResponse,
-  UserMutationResponse,
-  MeQueryResponse,
-} from "../graphql/types";
+import { VariablesOf } from "../graphql/tada";
 
 export const getMe = async () => {
-  const response = await graphqlClient.request<MeQueryResponse>(ME_QUERY);
+  const response = await graphqlClient.request(ME_QUERY);
   return response;
 };
 
 export const getUser = async (id: string) => {
-  const response = await graphqlClient.request<UserQueryResponse>(USER_QUERY, {
+  const response = await graphqlClient.request(USER_QUERY, {
     id,
   });
   return response;
 };
 
-export const updateMe = async (input: UpdateUserInput) => {
-  const response = await graphqlClient.request<{
-    updateMe: UserMutationResponse;
-  }>(UPDATE_ME_MUTATION, { input });
+export const updateMe = async (
+  input: VariablesOf<typeof UPDATE_ME_MUTATION>["input"]
+) => {
+  const response = await graphqlClient.request(UPDATE_ME_MUTATION, { input });
   return response.updateMe;
 };
