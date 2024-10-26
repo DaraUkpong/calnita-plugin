db22b7593f57a87ef9b28fb7aac06da690f73242
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-10-26 17:38:46 +0100

    added GET_PARTNER_BY_ID query

--- src/services/graphql/queries.ts
+++ src/services/graphql/queries.ts
@@ -95,3 +95,30 @@ export const USER_QUERY = graphql(`
     }
   }
 `);
+
+export const GET_PARTNER_BY_ID = graphql(`
+  query Partner($id: ID!) {
+    partner(id: $id) {
+      id
+      email
+      name
+      siteUrl
+      products {
+        id
+        name
+        category
+        price
+        imageUrl
+        suitableSkinTypes
+        targetedConcerns
+        ingredients
+        creator {
+          id
+          name
+          email
+          siteUrl
+        }
+      }
+    }
+  }
+`);
